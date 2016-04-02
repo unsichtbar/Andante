776bf02c71c03faf024a62cd70a7f577afba33e8
a-ns <exmann94@gmail.com>
2016-04-02 10:54:25 -0500

    Drawable Interface for things that are to be drawn

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -8,6 +8,7 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
 
+    //bloop bop
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
