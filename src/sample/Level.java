92c1467701e5b254848290a9316c4cf61520601b
Mikayla <mtimm100@gmail.com>
2016-04-02 11:33:25 -0500

    test

---
+++ src/sample/Level.java
@@ -0,0 +1,11 @@
+package sample;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Mikayla on 4/2/2016.
+ */
+public class Level {
+    private ArrayList<Question> questions;
+
+}
---
+++ src/sample/Question.java
@@ -0,0 +1,16 @@
+package sample;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Mikayla on 4/2/2016.
+ */
+public class Question {
+    private Card quest;
+    private ArrayList<Card> choices;
+    private Card correct;
+
+    public Question(String question, ){ //get these from the database
+
+    }
+}
